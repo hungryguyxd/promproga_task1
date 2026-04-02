b6c076b069683db110240ab1e95755179f835309
maga <maga@MacBook-Pro-maga.local>
2026-04-02 23:41:20 +0300

    change

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+eval $(minikube docker-env)
+docker build -t lol ./src
+kubectl apply -f kubik/configmap.yaml
+
+kubectl apply -f kubik/pod.yaml
+kubectl wait --for=condition=ready pod/lolpod --timeout=60s
+kubectl apply -f kubik/deployment.yaml
+kubectl rollout restart deployment lol
+
+kubectl apply -f kubik/service.yaml
+kubectl apply -f kubik/daemonset.yaml
+kubectl apply -f kubik/cronjob.yaml
+kubectl apply -f kubik/statefulset.yaml
+
+kubectl rollout status statefulset/lol-stateful
+kubectl rollout status deployment/lol
